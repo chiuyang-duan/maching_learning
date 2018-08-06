3daa79a193048709aa2fa3a92588f33590d2c314
desep <631735354@qq.com>
2018-08-06 23:23:35 +0800

    finish the framework v0.1

--- 1_neural_network/1-2_BP_multi-layer_feedforward_neural/Common.h
+++ 1_neural_network/1-2_BP_multi-layer_feedforward_neural/Common.h
@@ -10,8 +10,19 @@
 #define OUT_NUM 2
 #define HIDDEN_LAYER_NUM 4
 
+struct delta_parameters{
+
+    double * w;
+    double * theta;
+    
+    double * v;
+    double * gama;
+    
+};
+
 struct input_data{
     double * x;
+	double * y;
     int input_num; //none dummy node
     int out_num;   //none dummy node
     int hidden_layer_num;  //none dummy node
