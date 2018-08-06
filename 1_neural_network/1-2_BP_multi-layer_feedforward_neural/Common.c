d5107e7f4945a4371b79c446677ff21026b991d2
qiuyang.duan <qiuyang.duan@tcl.com>
2018-08-06 19:32:27 +0800

    init data (malloc && free)

--- 1_neural_network/1-2_BP_multi-layer_feedforward_neural/Common.c
+++ 1_neural_network/1-2_BP_multi-layer_feedforward_neural/Common.c
@@ -12,7 +12,31 @@ double act_step_function(double x)
 	return 0;
 }
 
+void normal_zscore(double * inputdata, int length, double * outdata)
+{
+    double u = 0;
+    double sigma = 0; 
+    double a = 0;
+    int i =0;
 
+    outdata = (double *)malloc(length*sizeof(double));     
+    
+    for(i = 0; i < length; i++)
+    {
+        u = u + inputdata[i];            
+    }
+    u = u / length;
+    for(i = 0; i < length; i++)
+    {
+        a = inputdata[i] - u;
+        sigma = sigma + (a * a);
+    }
+    sigma = sigma / length;
+    for(i = 0; i < length; i++)
+    {
+        outdata[i] = (inputdata[i] - u) / sigma;   
+    }
+}
 
 
 
