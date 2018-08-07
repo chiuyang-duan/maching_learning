3974712d9c8020610d3da7f20a2d06c0fac5c48c
desep <631735354@qq.com>
2018-08-07 19:20:29 +0800

    BP_neural_network_v1.0 The work has been completed in the main

--- 1_neural_network/1-2_BP_multi-layer_feedforward_neural/feedforward_neural.c
+++ 1_neural_network/1-2_BP_multi-layer_feedforward_neural/feedforward_neural.c
@@ -15,15 +15,23 @@ aim in triangle:
 double data_x[2][1500];
 double data_out[2][1500]; 
 
+void rand_init()
+{
+    srand((int)time(0));  
+}
+double rand_num(double max)
+{
+    return (rand()/double(RAND_MAX)* 2 * max) - max;
+}
+
 void make_study_data()
 {
 
     int i;
-    srand((int)time(0));
     for(i = 0;i<1500;i++)
     {
-        data_x[0][i] = (rand()/double(RAND_MAX)*20)-10;
-        data_x[1][i] = (rand()/double(RAND_MAX)*20)-10;
+        data_x[0][i] = rand_num(10);
+        data_x[1][i] = rand_num(10);
         
         if((data_x[1][i] < data_x[0][i])&&(data_x[1][i] > 0)&&(data_x[0][i] < 5))
         {
@@ -64,24 +72,48 @@ void init_data(struct input_data * datain, struct output_data * dataout)
     memset(dataout->para_v,0,((datain->input_num + 1) * datain->hidden_layer_num) * sizeof(double));
     memset(dataout->para_w,0,datain->out_num * (datain->hidden_layer_num + 1)*sizeof(double));    
     memset(dataout->y,0,(datain->out_num) * sizeof(double));
+
+
+    for(i = 0; i< datain->hidden_layer_num; i++)
+    {
+        for(j = 0;j < datain->input_num; j++)
+        {
+            dataout->para_v[(i * (datain->input_num + 1)) + j] = rand_num(1);            
+        } 
+        dataout->para_v[(i * datain->input_num) + j] = rand_num(1);
+    }
+    for(i = 0; i< datain->out_num; i++)
+    {
+        for(j = 0;j < datain->hidden_layer_num; j++)
+        {
+            dataout->para_w[(i * (datain->hidden_layer_num + 1)) + j] = rand_num(1);            
+        } 
+        dataout->para_w[(i * datain->hidden_layer_num) + j] = rand_num(1); 
+    }   
     
 }
 
 void init_delta_para(struct input_data * datain, struct delta_parameters * delta_para)
 {
     delta_para = (delta_parameters *)malloc(sizeof(delta_parameters));
+
     delta_para->v = (double *)malloc(datain->input_num * datain->hidden_layer_num * sizeof(double)); 
     delta_para->theta = (double *)malloc(datain->hidden_layer_num * sizeof(double));
         
     delta_para->w = (double *)malloc(datain->out_num * datain->hidden_layer_num * sizeof(double)); 
     delta_para->gama = (double *)malloc(datain->out_num * sizeof(double));
+
+    delta_para->gradient = (double *)malloc(datain->out_num * sizeof(double));  
     
     memset(datain,0,sizeof(input_data));    
+    
     memset(delta_para->v,0,datain->input_num * datain->hidden_layer_num * sizeof(double)); 
     memset(delta_para->theta,0,sizeof(datain->hidden_layer_num * sizeof(double)));
     
     memset(delta_para->w,0,datain->out_num * datain->hidden_layer_num * sizeof(double));   
     memset(delta_para->gama,0,sizeof(datain->out_num * sizeof(double)));
+    
+    memset(delta_para->gradient,0,sizeof(datain->out_num * sizeof(double)));
 }
 void free_delta_para(struct input_data * datain, struct delta_parameters * delta_para)
 {
@@ -90,8 +122,11 @@ void free_delta_para(struct input_data * datain, struct delta_parameters * delta
     
     free(delta_para->w);
     free(delta_para->gama);
+
+    free(delta_para->gradient);
     
-    free(datain);    
+    free(datain); 
+    
 }
 void free_data(struct input_data * datain, struct output_data * dataout)
 {
@@ -101,6 +136,7 @@ void free_data(struct input_data * datain, struct output_data * dataout)
     free(dataout->y);  
     free(dataout);
     free(datain->x);
+    free(datain->y);
     free(datain);
 }
 /*
@@ -113,7 +149,7 @@ void bp_function(struct input_data * datain, struct output_data * dataout)
     {
         for(j = 0;j < datain->input_num;j++)
         {
-            dataout->hidden_b[i] = dataout->hidden_b[i] + datain->x[j] * dataout->para_v[(i * datain->input_num)+j];            
+            dataout->hidden_b[i] = dataout->hidden_b[i] + datain->x[j] * dataout->para_v[(i * (datain->input_num + 1))+j];            
         } 
         dataout->hidden_b[i] = act_squashing_function(dataout->hidden_b[i] + (-1.0) * dataout->para_v[(i * datain->input_num)+j]);
     }
@@ -121,39 +157,57 @@ void bp_function(struct input_data * datain, struct output_data * dataout)
     {
         for(j = 0;j < datain->hidden_layer_num;j++)
         {
-            dataout->y[i] = dataout->y[i] + dataout->hidden_b[j] * dataout->para_w[(i * datain->hidden_layer_num)+j];            
+            dataout->y[i] = dataout->y[i] + dataout->hidden_b[j] * dataout->para_w[(i * (datain->hidden_layer_num + 1))+j];            
         } 
         dataout->y[i] = act_squashing_function(dataout->y[i] + (-1.0) * dataout->para_w[(i * datain->hidden_layer_num)+j]); 
     }    
 }
 void para_iteration(struct input_data * datain, struct output_data * dataout, struct delta_parameters * delta_para)
 {
-    int i,j;
-	double gradient;
-	
-    for(i = 0; i< datain->out_num;i++)
+    int i,j,k;
+    double sum_w_g = 0;
+	double eh = 0;
+    for(i = 0; i < datain->out_num;i++)
     {
-    	gradient = dataout->y[i] * (1 - dataout->y[i]) * (datain->y[i] - dataout->y[i]);
+    	delta_para->gradient[i] = dataout->y[i] * (1 - dataout->y[i]) * (datain->y[i] - dataout->y[i]);
         for(j = 0;j < datain->hidden_layer_num;j++)
         {
-            delta_para->w[(i * datain->hidden_layer_num)+j] = 
-				LEARNING_RATE1 * gradient * dataout->hidden_b[j];
-            
+            delta_para->w[(i * datain->hidden_layer_num)+j] = LEARNING_RATE1 * delta_para->gradient[i] * dataout->hidden_b[j];            
         } 
-    }   
-	
+        delta_para->theta[i] = (-1) * LEARNING_RATE1 * delta_para->gradient[i];
+    }
+    
+    for(i = 0; i < datain->hidden_layer_num;i++)
+    {  
+        for(k = 0; k < datain->out_num; k++)
+        {
+            sum_w_g = sum_w_g + (delta_para->gradient[k] * dataout->para_w[k * datain->hidden_layer_num + i]);             
+        }
+        eh = dataout->hidden_b[i] * (1 - dataout->hidden_b[i]) * sum_w_g;
+        for(j = 0;j < datain->input_num;j++)
+        {                
+            delta_para->v[i * datain->input_num + j] = LEARNING_RATE1 * eh * datain->x[j];
+        } 
+        delta_para->gama[i] = (-1) * LEARNING_RATE1 * eh;
+    }
+
+
     for(i = 0; i< datain->hidden_layer_num;i++)
     {
         for(j = 0;j < datain->input_num;j++)
         {
-            delta_para->v[(i * datain->input_num)+j] =        
-
-            
-
-            
+             dataout->para_v[(i * (datain->input_num + 1)) + j] = dataout->para_v[(i * (datain->input_num + 1)) + j] + delta_para->v[(i * datain->input_num) + j];            
         } 
+        dataout->para_v[(i * datain->input_num) + j]) = dataout->para_v[(i * datain->input_num) + j]) + delta_para->gama[i];
     }
-
+    for(i = 0; i< datain->out_num;i++)
+    {
+        for(j = 0;j < datain->hidden_layer_num;j++)
+        {
+            dataout->para_w[(i * (datain->hidden_layer_num + 1)) + j] = dataout->para_w[(i * (datain->hidden_layer_num + 1)) + j] + delta_para->w[(i * datain->hidden_layer_num) +j];            
+        } 
+        dataout->para_w[(i * datain->hidden_layer_num)+j]) = dataout->para_w[(i * datain->hidden_layer_num)+j]) + delta_para->theta[i]; 
+    }    
 }
 
 int main()
@@ -163,45 +217,47 @@ int main()
     struct input_data * datain;
     struct output_data * dataout;
     struct delta_parameters * delta_para;
-
+    
+    rand_init();
     init_data(datain, dataout);
     init_delta_para(datain,delta_para);
+    make_study_data(); 
     
     for(j = 0; j < 1500; j++)
     {
         for(i = 0; i < 2; i++)
         {
             datain->x[i] = data_x[i][j];  
-			datain->y[]
+			datain->y[i] = data_out[i][j];
         }         
         bp_function(datain, dataout);
         para_iteration(datain, dataout, delta_para);
     }
     
+    while(1)
+    {
+        printf("       |        .    .       '\n");
+        printf("       |        .  .         '\n");
+        printf("       |        ..           '\n");
+        printf("       |       ..            '\n");
+        printf("       |     .++.            '\n");
+        printf("       |   .++++.            '\n");
+        printf("       | .++++++.            '\n");
+        printf("-------.--------.------------'\n");
+        printf("     . |        .            '\n");
+        printf("   .   |        .            '\n");
+        printf(" .     |        .            '\n");    
+        printf("pls input test data 'input_x1,input_x2'\n");
+        scanf("%lf,%lf",input_x[0],input_x[1]);
 
-
-
-
-    
-    printf("       |        .    .       '\n");
-    printf("       |        .  .         '\n");
-    printf("       |        ..           '\n");
-    printf("       |       ..            '\n");
-    printf("       |     .++.            '\n");
-    printf("       |   .++++.            '\n");
-    printf("       | .++++++.            '\n");
-    printf("-------.--------.------------'\n");
-    printf("     . |        .            '\n");
-    printf("   .   |        .            '\n");
-    printf(" .     |        .            '\n");
-    
-    
-    printf("pls input test data 'input_x1,input_x2'\n");
-    scanf("%lf,%lf",input_x[0],input_x[1]);
-
+        datain->x[0] = input_x[0];
+        datain->x[1] = input_x[1];
+        bp_function(datain, dataout);
+        printf("%lf,%lf\n",dataout->y[0],dataout->y[1]);
+        
+    }
 
     free_delta_para(datain, delta_para)
-
     free_data(datain, dataout)
         
 	return 0;
