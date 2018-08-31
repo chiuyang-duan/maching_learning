5247c7fedb62c10c437437e65a613e24824ec47e
desep <631735354@qq.com>
2018-08-31 16:52:16 +0800

    Begin v2.0

---
+++ 1_neural_network/3_Adapt_multilayer_actfun_v2.0/Common.h
@@ -0,0 +1,58 @@
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <malloc.h>
+#include <memory.h>
+#include <string.h>
+
+#define STUDY1 1
+
+#define INPUT_NUM 2
+#define OUT_NUM 2
+
+#define HIDDEN_LAYER_NUM 4
+
+#define TEST_NUM 30000
+#define CODE_VERSION "1.2 20180809"
+
+struct delta_parameters{
+    double * gradient;
+
+    double * w;
+    double * theta;
+    
+    double * v;
+    double * gama;
+    
+};
+
+struct input_data{
+    double * x;
+	double * y;
+    int input_num; //none dummy node
+    int out_num;   //none dummy node
+    int hidden_layer_num;  //none dummy node
+};
+
+struct output_data{
+    double * para_w;
+    double * para_v;
+    double * hidden_b;
+    double * y;
+};
+struct pdata{
+    struct input_data       * pin;
+    struct output_data      * pout;
+    struct delta_parameters * pdelta;
+};
+
+double act_squashing_function(double x);
+double act_step_function(double x);
+void normal_zscore(double * inputdata, int length, double * outdata);
+
+ 
+#endif
