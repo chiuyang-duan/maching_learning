2e93a676db55e5c161490d5901375348fe5e859d
desep <631735354@qq.com>
2018-08-31 19:27:56 +0800

    reconfiguration

--- 1_neural_network/3_Adapt_multilayer_actfun_v2.0/Common.h
+++ 1_neural_network/3_Adapt_multilayer_actfun_v2.0/Common.h
@@ -9,50 +9,13 @@
 #include <memory.h>
 #include <string.h>
 
-#define STUDY1 1
-
-#define INPUT_NUM 2
-#define OUT_NUM 2
-
-#define HIDDEN_LAYER_NUM 4
-
-#define TEST_NUM 30000
-#define CODE_VERSION "1.2 20180809"
-
-struct delta_parameters{
-    double * gradient;
-
-    double * w;
-    double * theta;
-    
-    double * v;
-    double * gama;
+struct neural_arch{
+    int input_node_num;
+    int output_node_num;
+    int hidden_layer_num;
+    int * hidden_layer_node_num;
+    void (*get_arg)(struct * neural_arch);
     
 };
 
-struct input_data{
-    double * x;
-	double * y;
-    int input_num; //none dummy node
-    int out_num;   //none dummy node
-    int hidden_layer_num;  //none dummy node
-};
-
-struct output_data{
-    double * para_w;
-    double * para_v;
-    double * hidden_b;
-    double * y;
-};
-struct pdata{
-    struct input_data       * pin;
-    struct output_data      * pout;
-    struct delta_parameters * pdelta;
-};
-
-double act_squashing_function(double x);
-double act_step_function(double x);
-void normal_zscore(double * inputdata, int length, double * outdata);
-
- 
 #endif
