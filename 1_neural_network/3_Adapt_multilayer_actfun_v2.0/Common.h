96c81ada03fc3de8a3b44fbb519941256069c728
desep <631735354@qq.com>
2018-09-03 19:36:45 +0800

    use two-way linked-list

--- 1_neural_network/3_Adapt_multilayer_actfun_v2.0/Common.h
+++ 1_neural_network/3_Adapt_multilayer_actfun_v2.0/Common.h
@@ -17,12 +17,14 @@ struct neural_node
     double *weight;
     double *delta_weight;
     double out;   
+    struct neural_node * prev_node;
     struct neural_node * next_node;
 };
 
 struct neural_layer
 {
     struct neural_node * node; 
+    struct neural_layer * prev_layer;
     struct neural_layer * next_layer;
 };
 
@@ -36,7 +38,7 @@ struct neural_context
 {
     struct neural_arg * arg;
     struct neural_layer * layer;
-    int (*init)(struct neural_arg * ,struct neural_layer *);
+    int (*init)(struct neural_arg * ,struct neural_layer * );
     int (*run)(struct neural_arg * ,struct neural_layer *);   
     int (*get_delta)(struct neural_arg *);
     int (*para_iteration)(struct neural_arg *);
